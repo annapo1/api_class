7ce5fcac13ad7aa10e907dc39c33da6911bacad9
frost <fxtrdnsk@gmail.com>
2015-06-06 11:38:05 -0700

    test_path added

--- api_framework/src/pubapiutils.py
+++ api_framework/src/pubapiutils.py
@@ -15,16 +15,17 @@ class Config:
         self.admin_login = self.parser.get('Server', 'user')
         self.password = self.parser.get('Server', 'passwd')
         self.puser = self.parser.get('Server', 'puser')
+        self.testpath = self.parser.get('Server', 'testpath')
 
 
 class Calls:
     def __init__(self):
-        pass
+        self.config = Config()
 
     def create_folder(self, folder_name, path=None, domain=None, method=None, content_type=None, accept=None,
                       username=None, password=None):
         if domain is None:
-            domain = 'https://istepanko.qa-egnyte.com'
+            domain = self.config.domain
         if method is None:
             method = 'POST'
         if content_type is None:
@@ -32,11 +33,11 @@ class Calls:
         if accept is None:
             accept = 'application/json'
         if username is None:
-            username = 'istepanko'
+            username = self.config.admin_login
         if password is None:
-            password = 'egnyte4you'
+            password = self.config.password
         if path is None:
-            path = '/Shared/test_folder/'
+            path = self.config.testpath
 
         endpoint = '/public-api/v1/fs'
         url = '%s%s%s%s' % (domain, endpoint, path, folder_name)
@@ -66,7 +67,7 @@ class Calls:
     def delete_folder(self, folder_path, domain=None, method=None, content_type=None, accept=None,
                       username=None, password=None):
         if domain is None:
-            domain = 'https://istepanko.qa-egnyte.com'
+            domain = self.config.domain
         if method is None:
             method = 'DELETE'
         if content_type is None:
@@ -74,9 +75,9 @@ class Calls:
         if accept is None:
             accept = 'application/json'
         if username is None:
-            username = 'istepanko'
+            username = self.config.admin_login
         if password is None:
-            password = 'egnyte4you'
+            password = self.config.password
 
         endpoint = '/public-api/v1/fs'
         url = '%s%s%s' % (domain, endpoint, folder_path)
