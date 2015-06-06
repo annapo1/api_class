923df21966c473646372064db11b911fc22b9947
frost <fxtrdnsk@gmail.com>
2015-06-06 10:50:48 -0700

    test

--- api_framework/src/pubapiutils.py
+++ api_framework/src/pubapiutils.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from ConfigParser import SafeConfigParser
 
 
 def create_folder(folder_name, path=None, domain=None, method=None, content_type=None, accept=None,
@@ -33,6 +34,43 @@ def create_folder(folder_name, path=None, domain=None, method=None, content_type
         url=url,
         headers=headers,
         data=data,
+        auth=(username, password)
+    )
+
+    try:
+        json_resp = json.loads(r.content)
+    except ValueError:
+        json_resp = 'NoJSON'
+
+    r.json = json_resp
+    return r
+
+
+def delete_folder(folder_path, domain=None, method=None, content_type=None, accept=None,
+                  username=None, password=None):
+    if domain is None:
+        domain = 'https://istepanko.qa-egnyte.com'
+    if method is None:
+        method = 'DELETE'
+    if content_type is None:
+        content_type = 'application/json'
+    if accept is None:
+        accept = 'application/json'
+    if username is None:
+        username = 'istepanko'
+    if password is None:
+        password = 'egnyte4you'
+
+    endpoint = '/public-api/v1/fs'
+    url = '%s%s%s' % (domain, endpoint, folder_path)
+    headers = dict()
+    headers['Content-Type'] = content_type
+    headers['Accept'] = accept
+
+    r = requests.request(
+        method=method,
+        url=url,
+        headers=headers,
         auth=(username, password)
     )
 
--- api_framework/tests/pubapi/testPubCreateFolder.py
+++ api_framework/tests/pubapi/testPubCreateFolder.py
@@ -2,13 +2,19 @@ import pubapiutils
 import httplib
 
 
-def test_create_100_folders_positive():
+def test_create_5_folders_positive():
     no_json = 'NoJSON'
+    folder_path = '/Shared/test_folder/'
+    l = []
     for i in range(5):
-        folder_name = 'test_folder123%s' % i
+        folder_name = 'test_folder%s' % i
         resp = pubapiutils.create_folder(folder_name)
         assert resp.status_code == httplib.CREATED
         assert resp.json == no_json
+        l.append(folder_name)
+    for item in l:
+        resp = pubapiutils.delete_folder(folder_path=folder_path + item)
+        assert resp.status_code == httplib.OK
 
 
 def test_create_folders_positive():
