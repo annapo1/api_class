fb4f904b117cd510ebc5888bb7a0e513f6966a8e
annapo1 <annapo1@users.noreply.github.com>
2015-06-13 13:11:31 -0700

    Update testPubMoveItem.py

--- api_framework/tests/pubapi/testPubMoveItem.py
+++ api_framework/tests/pubapi/testPubMoveItem.py
@@ -40,3 +40,26 @@ class TestClass(TestCase):
         resp = self.calls.move_item(name=folder2, destination=folder1_path, parent_path=self.config.testpath)
         assert resp.status_code == httplib.NOT_FOUND
         assert resp.json['errorMessage'] == "Source path for move doesn't exist"
+        
+    def test_move_folder_permissions_all(self):
+        perms ={'None': 0, 'Viewer': 1, 'Editor': 2, 'Full' = 3, 'Owener' = 4}
+        source_folder = self.utils.random_name()
+        target_folder = self.utils.random_name()
+        source_folder_path = '%s/%s' % (self.config.testpath, source_folder)
+        target_folder_path = '%s/%s' % (self.config.testpath, target_folder)
+        for perm1 in perms:
+            for perm2 in perms:
+                self.calls.create_folder(source_folder)
+                self.calls.create_folder(target_folder)
+                self.calls.set_perms(folder_path=source_folder_path, permission=perm1, users=self.config.puser)
+                self.calls.set_perms(folder_path=source_folder_path, permission=perm2, users=self.config.puser)
+                resp = self.calls.move_item(name=source_folder, destination=target)
+                if perms[perm1] > 2 and perms[perm2] > 1:
+                    assert resp.status_code = httplib.OK
+                    assert resp.json == self.no_json
+                else:
+                    assert resp.status_code = httplib.FORBIDDEN
+                    assert True
+                self.calls.delete_folder(name='', parent_path=self
+        
+        
